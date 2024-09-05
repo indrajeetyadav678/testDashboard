b50142fd87e23b6f69b44df81594c3d6bb42e4d1
Indrajeet <yindrajeet678@gmail.com>
2024-09-05 23:58:49 +0530

    tvdashboard

---
+++ src/Dashboard1.js
@@ -0,0 +1,31 @@
+const Dashboard1 = () => {
+  return (
+    <div>
+      <div id="Titlediv">
+        <div id="Title">InttelliSynce</div>
+        <div id="Title">Sewing Dashboard</div>
+        <div id="Title">Time: 12:00 PM</div>
+      </div>
+      <div id="headings">
+        <div id="top_heading">Buyer</div>
+        <div id="top_heading">Style</div>
+        <div id="top_heading">Ourref</div>
+        <div id="top_heading">Color</div>
+      </div>
+      <div id="top_part">
+        <div id="top_front_div1">Fontend-1</div>
+        <div id="top_front_div1">Fontend-2</div>
+        <div id="top_front_div1">Fontend-3</div>
+        <div id="top_front_div1">Fontend-4</div>
+      </div>
+      <div id="bottum_part">
+        <div id="bottum_div1">Bottum-1</div>
+        <div id="bottum_div1">Bottum-2</div>
+        <div id="bottum_div1">Bottum-3</div>  
+        <div id="bottum_div1">Bottum-4</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard1;
